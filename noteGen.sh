a5108300d8e55c0dd5ceedc497a757b4e9c20b4f
Baj Ivan <ivanli@dev>
2020-08-11 15:01:17 +0200

    prototype with appending works

--- noteGen.sh
+++ noteGen.sh
@@ -1,19 +1,17 @@
-#!/bin/sh
+#/bin/sh
 
 NOTE_NAME="${1}"
 CURRENT_DATE_YYMMDD=$(date +%g%m%d)
 FILE_NAME="${CURRENT_DATE_YYMMDD}"_"${NOTE_NAME}"
 if [ -f "${FILE_NAME}" ]; then
-cat <<EO1 >> "${FILE_NAME}"
-=======Appending========
-
-EO1 
-
+echo "==========Appending========" >> "${FILE_NAME}"
 else
 cat <<EOF > "${FILE_NAME}"
-**********Note************
-Created "${CURRENT_DATE_YYMMDD}" {YYMMDD}
-**************************
+	**********Note************
+	Created ${CURRENT_DATE_YYMMDD} (YYMMDD)
+	**************************
 EOF
 fi
 vi "${FILE_NAME}"
+
+
